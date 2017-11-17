8d83ce41918144424f884533491b13f0453fa569
PabloFB18 <pfb006@alumnos.ucn.cl>
2017-11-17 19:43:20 -0300

    visitor terminado

--- visitor.py
+++ visitor.py
@@ -13,8 +13,9 @@ class Visitor(object):
         self.id_expression = 0
         self.id_var = 0
         self.id_simple_expression = 0
-        self.id_aditive_expression = 0
+        self.id_additive_expression = 0
         self.id_term = 0
+        self.id_num = 0
         self.id_call = 0
 
     # def visit_null(self, null):
@@ -23,25 +24,28 @@ class Visitor(object):
 
     def visit_program(self, program):
         self.id_program += 1
+        id_program = self.id_program
         for declaration in program.declarations_p:
-            self.ast += '\t"program' + str(self.id_program) + '" '
+            self.ast += '\t"program' + str(id_program) + '" '
             declaration.accept(self)
         self.ast = 'digraph G {\n' + self.ast + '}'
 
     def visit_var_declaration(self, var_declaration):
         self.id_var_declaration += 1
+        id_var_declaration = self.id_var_declaration
         if not var_declaration.numero_si_no:
-            var = '-> ' + '"var' + str(self.id_var_declaration) + ': ' + var_declaration.type_specifier_t + ' ' + \
+            var = '-> ' + '"var' + str(id_var_declaration) + ': ' + var_declaration.type_specifier_t + ' ' + \
                         var_declaration.id_t + '"'
             self.ast += var + '\n'
         else:
-            var = '-> ' + '"var' + str(self.id_var_declaration) + ': ' + var_declaration.type_specifier_t + ' ' + \
+            var = '-> ' + '"var' + str(id_var_declaration) + ': ' + var_declaration.type_specifier_t + ' ' + \
                         var_declaration.id_t + '[' + var_declaration.num_t + ']"'
             self.ast += var + '\n'
 
     def visit_fun_declaration(self, fun_declaration):
         self.id_fun_declaration += 1
-        fun = '"function' + str(self.id_fun_declaration) + ': ' + fun_declaration.type_specifier_t + ' ' + \
+        id_fun_declaration = self.id_fun_declaration
+        fun = '"function' + str(id_fun_declaration) + ': ' + fun_declaration.type_specifier_t + ' ' + \
             fun_declaration.id_t + '"'
         self.ast += '-> ' + fun + '\n'
         if fun_declaration.compound_stmt_p.local_declarations_p is not None or \
@@ -55,84 +59,127 @@ class Visitor(object):
 
     def visit_param(self, param):
         self.id_param += 1
+        id_param = self.id_param
         if param.arreglo_si_no:
-            param = '-> ' + '"param' + str(self.id_param) + ': ' + param.type_specifier_t + ' ' + \
+            param = '-> ' + '"param' + str(id_param) + ': ' + param.type_specifier_t + ' ' + \
                   param.id_t + '"'
             self.ast += param + '\n'
         else:
-            param = '-> ' + '"param' + str(self.id_param) + ': ' + param.type_specifier_t + '[] ' + \
+            param = '-> ' + '"param' + str(id_param) + ': ' + param.type_specifier_t + '[] ' + \
                   param.id_t + '"'
             self.ast += param + '\n'
 
     def visit_compound_stmt(self, compound_stmt):
         self.id_compound_stmt += 1
-        self.ast += '-> ' + '"compound_stmt' + str(self.id_compound_stmt) + '"' + '\n'
+        id_compound_stmt = self.id_compound_stmt
+        self.ast += '-> ' + '"compound_stmt' + str(id_compound_stmt) + '"' + '\n'
         if compound_stmt.local_declarations_p is not None:
             for local_declaration in compound_stmt.local_declarations_p:
                 if local_declaration is not None:
-                    self.ast += '\t"compound_stmt' + str(self.id_compound_stmt) + '" '
+                    self.ast += '\t"compound_stmt' + str(id_compound_stmt) + '" '
                     local_declaration.accept(self)
         if compound_stmt.stmt_list_p is not None:
             for stmt in compound_stmt.stmt_list_p:
                 if stmt is not None:
-                    self.ast += '\t"compound_stmt' + str(self.id_compound_stmt) + '" '
+                    self.ast += '\t"compound_stmt' + str(id_compound_stmt) + '" '
                     stmt.accept(self)
 
-    def visit_expression_stmt(self, expresion_stmt):
-        self.id_expression_stmt += 1
-        self.ast += '-> ' + '"expresion' + str(self.id_expression_stmt) + '"' + '\n'
-        self.ast += '\t"expresion' + str(self.id_expression_stmt) + '" '
-        expresion_stmt.expresion_p.accept(self)
+    # def visit_expression_stmt(self, expresion_stmt):
+    #     self.id_expression_stmt += 1
+    #     id_expression_stmt = self.id_expression_stmt
+    #     self.ast += '-> ' + '"expresion_stmt' + str(id_expression_stmt) + '"' + '\n'
+    #     self.ast += '\t"expresion_stmt' + str(id_expression_stmt) + '" '
+    #     expresion_stmt.expresion_p.accept(self)
 
     def visit_selection_stmt(self, selection_stmt):
+        self.id_selection_stmt += 1
+        id_selection_stmt = self.id_selection_stmt
         if not selection_stmt.else_si_no:
-            self.id_selection_stmt += 1
-            self.ast += '-> "' + 'if' + str(self.id_selection_stmt) + '"' + '\n'
-            self.ast += '\t"' + 'if' + str(self.id_selection_stmt) + '" '
-            selection_stmt.expresion_p.accept(self)
-            self.ast += '\t"' + 'if' + str(self.id_selection_stmt) + '" '
+            self.ast += '-> "' + 'if' + str(id_selection_stmt) + '"' + '\n'
+            self.ast += '\t"' + 'if' + str(id_selection_stmt) + '" '
+            selection_stmt.expression_p.accept(self)
+            self.ast += '\t"' + 'if' + str(id_selection_stmt) + '" '
             selection_stmt.stmt_p.accept(self)
         else:
-            self.id_selection_stmt += 1
-            self.ast += '-> "' + 'if_else' + str(self.id_selection_stmt) + '"' + '\n'
-            self.ast += '\t"' + 'if_else' + str(self.id_selection_stmt) + '" '
-            selection_stmt.expresion_p.accept(self)
-            self.ast += '\t"' + 'if_else' + str(self.id_selection_stmt) + '" '
+            self.ast += '-> "' + 'if_else' + str(id_selection_stmt) + '"' + '\n'
+            self.ast += '\t"' + 'if_else' + str(id_selection_stmt) + '" '
+            selection_stmt.expression_p.accept(self)
+            self.ast += '\t"' + 'if_else' + str(id_selection_stmt) + '" '
             selection_stmt.stmt_p.accept(self)
-            self.ast += '\t"' + 'if_else' + str(self.id_selection_stmt) + '" '
+            self.ast += '\t"' + 'if_else' + str(id_selection_stmt) + '" '
             selection_stmt.stmt2_p.accept(self)
 
     def visit_iteration_stmt(self, iteration_stmt):
         self.id_iteration_stmt += 1
-            # self.ast += '-> "' + 'if' + str(self.id_selection_stmt) + '"' + '\n'
-            # self.ast += '\t"' + 'if' + str(self.id_selection_stmt) + '" '
-            # selection_stmt.expresion_p.accept(self)
-            # self.ast += '\t"' + 'if' + str(self.id_selection_stmt) + '" '
-            # selection_stmt.stmt_p.accept(self)
+        id_iteration_stmt = self.id_iteration_stmt
+        self.ast += '-> "' + 'while' + str(id_iteration_stmt) + '"' + '\n'
+        self.ast += '\t"' + 'while' + str(id_iteration_stmt) + '" '
+        iteration_stmt.expression_p.accept(self)
+        self.ast += '\t"' + 'while' + str(id_iteration_stmt) + '" '
+        iteration_stmt.stmt_p.accept(self)
 
     def visit_return_stmt(self, return_stmt):
         self.id_return_stmt += 1
+        id_return_stmt = self.id_return_stmt
+        self.ast += '-> "' + 'return' + str(id_return_stmt) + '"' + '\n'
+        if not return_stmt.expression_si_no:
+            self.ast += '\t"' + 'return' + str(id_return_stmt) + '" '
+            return_stmt.expression_p.accept(self)
 
     def visit_expression(self, expression):
         self.id_expression += 1
-        pass
+        id_expression = self.id_expression
+        self.ast += '-> "' + 'assign' + str(id_expression) + '"' + '\n'
+        self.ast += '\t"' + 'assign' + str(id_expression) + '" '
+        expression.var_p.accept(self)
+        self.ast += '\t"' + 'assign' + str(id_expression) + '" '
+        expression.expression_p.accept(self)
 
     def visit_var(self, var):
         self.id_var += 1
-        pass
+        id_var = self.id_var
+        self.ast += '-> "' + 'var' + str(id_var) + ': ' + var.id_t + '"' + '\n'
+        if var.expression_si_no:
+            self.ast += '\t"' + 'var' + str(id_var) + ': ' + var.id_t + '" '
+            var.expression_p.accept(self)
 
     def visit_simple_expression(self, simple_expresion):
         self.id_simple_expression += 1
-        pass
+        id_simple_expression = self.id_simple_expression
+        self.ast += '-> "COMPARADOR' + str(id_simple_expression) + ': ' + simple_expresion.relop_t + '"' + '\n'
+        self.ast += '\t"COMPARADOR' + str(id_simple_expression) + ': ' + simple_expresion.relop_t + '" '
+        simple_expresion.additive_expression1_p.accept(self)
+        self.ast += '\t"COMPARADOR' + str(id_simple_expression) + ': ' + simple_expresion.relop_t + '" '
+        simple_expresion.additive_expression2_p.accept(self)
 
-    def visit_aditive_expression(self, aditive_expresion):
-        self.id_aditive_expression += 1
-        pass
+    def visit_additive_expression(self, additive_expresion):
+        self.id_additive_expression += 1
+        id_additive_expression = self.id_additive_expression
+        self.ast += '-> "SIGNO' + str(id_additive_expression) + ': ' + additive_expresion.addop_t + '"' + '\n'
+        self.ast += '\t"SIGNO' + str(id_additive_expression) + ': ' + additive_expresion.addop_t + '" '
+        additive_expresion.additive_expression_p.accept(self)
+        self.ast += '\t"SIGNO' + str(id_additive_expression) + ': ' + additive_expresion.addop_t + '" '
+        additive_expresion.term_p.accept(self)
 
     def visit_term(self, term):
         self.id_term += 1
-        pass
+        id_term = self.id_term
+        self.ast += '-> "SIGNO' + str(id_term) + ': ' + term.mulop_t + '"' + '\n'
+        self.ast += '\t"SIGNO' + str(id_term) + ': ' + term.mulop_t + '" '
+        term.term_p.accept(self)
+        self.ast += '\t"SIGNO' + str(id_term) + ': ' + term.mulop_t + '" '
+        term.factor_p.accept(self)
+
+    def visit_num(self, num):
+        self.id_num += 1
+        id_num = self.id_num
+        self.ast += '-> "NUM' + str(id_num) + ': ' + num.num_t + '"' + '\n'
 
     def visit_call(self, call):
         self.id_call += 1
-        pass
+        id_call = self.id_call
+        self.ast += '-> "' + 'call' + str(id_call) + '"' + '\n'
+        for arg in call.args_p:
+            self.ast += '\t"' + 'call' + str(id_call) + '" '
+            arg.accept(self)
+
